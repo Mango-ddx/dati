895535bc66d1f232878d71c07dec11ee0d3164e6
xiagaojing <641612106@qq.com>
2020-05-12 11:55:01 +0800

    sql

---
+++ dati.sql
@@ -0,0 +1,183 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.18 : Database - dati_base
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dati_base` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `dati_base`;
+
+/*Table structure for table `group_topic` */
+
+DROP TABLE IF EXISTS `group_topic`;
+
+CREATE TABLE `group_topic` (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `group_name` varchar(120) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `class_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '对应的班级id',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `group_name` (`group_name`),
+  KEY `user_class_id` (`class_id`),
+  CONSTRAINT `group_topic_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `user_class` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `group_topic` */
+
+insert  into `group_topic`(`id`,`group_name`,`class_id`) values ('449e88eec7644baa9ab7f463e1f51b1e','test_0','10010'),('bf959eedb54943f2a5d368464e4cd686','test','10010');
+
+/*Table structure for table `permission` */
+
+DROP TABLE IF EXISTS `permission`;
+
+CREATE TABLE `permission` (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `prompt` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `permission_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `permission` */
+
+insert  into `permission`(`id`,`prompt`,`permission_name`) values ('#1','出题','problem'),('#2','其他人历史','history'),('#3','分配角色','role_manager');
+
+/*Table structure for table `permission_role` */
+
+DROP TABLE IF EXISTS `permission_role`;
+
+CREATE TABLE `permission_role` (
+  `permission_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`permission_id`,`role_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `permission_role_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `permission_role_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `permission_role` */
+
+insert  into `permission_role`(`permission_id`,`role_id`) values ('#1','854fc31c9fcc4421b446e72c1e53b000'),('#2','854fc31c9fcc4421b446e72c1e53b000'),('#3','854fc31c9fcc4421b446e72c1e53b000'),('#1','be9b59e0b06f470f8c14684c58a3a708'),('#2','be9b59e0b06f470f8c14684c58a3a708');
+
+/*Table structure for table `record` */
+
+DROP TABLE IF EXISTS `record`;
+
+CREATE TABLE `record` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `record_title` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '题目',
+  `record_answer` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '正确答案',
+  `user_answer` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户的答案',
+  `correct` int(11) NOT NULL COMMENT '是否正确,1正确,-1错误',
+  `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `group_topic_id` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `topics_id` int(11) NOT NULL,
+  `title_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `record_ibfk_3` (`topics_id`),
+  KEY `group_topic_id` (`group_topic_id`),
+  CONSTRAINT `record_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `record_ibfk_2` FOREIGN KEY (`group_topic_id`) REFERENCES `group_topic` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `record` */
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `prompt` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`role_name`,`prompt`) values ('854fc31c9fcc4421b446e72c1e53b000','class_manager','班级管理员'),('be9b59e0b06f470f8c14684c58a3a708','problem_history','出题和查询记录角色');
+
+/*Table structure for table `role_user` */
+
+DROP TABLE IF EXISTS `role_user`;
+
+CREATE TABLE `role_user` (
+  `role_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`role_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `role_user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `role_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `role_user` */
+
+insert  into `role_user`(`role_id`,`user_id`) values ('854fc31c9fcc4421b446e72c1e53b000','68323e27fe58425aa2b8857bc26ea40c');
+
+/*Table structure for table `topics` */
+
+DROP TABLE IF EXISTS `topics`;
+
+CREATE TABLE `topics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '题目',
+  `option_ans` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '选择数据',
+  `answer` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '正确答案',
+  `title_type` int(11) NOT NULL COMMENT '标识当前数据的题型',
+  `group_topic_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '对应题分类id',
+  PRIMARY KEY (`id`),
+  KEY `group_topic_id` (`group_topic_id`),
+  CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`group_topic_id`) REFERENCES `group_topic` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `topics` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `account` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '账号',
+  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `creationdate` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `class_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '对应的班级id',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`account`),
+  KEY `class_id` (`class_id`),
+  CONSTRAINT `class_id` FOREIGN KEY (`class_id`) REFERENCES `user_class` (`id`) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`account`,`password`,`creationdate`,`class_id`) values ('1294caa92a7c489f9f9af5e89dab07f9','夏高精','888','e5286db9b794193155b8639bf61d0d1b','2020-05-02 20:26:51','10010'),('68323e27fe58425aa2b8857bc26ea40c','admin','641612106','d7e0414211bed9210d3d9c4918959c57','2020-04-25 20:02:39','10010');
+
+/*Table structure for table `user_class` */
+
+DROP TABLE IF EXISTS `user_class`;
+
+CREATE TABLE `user_class` (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `class_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `explanation` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `class_name` (`class_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user_class` */
+
+insert  into `user_class`(`id`,`class_name`,`explanation`) values ('10010','401','401班级');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
